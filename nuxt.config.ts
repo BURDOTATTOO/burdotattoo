e913977613df65e575f1acf16b64d0a9f9a9739a
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-07-04 18:14:13 -0500

    adding config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: false },
+  css: ["~/assets/main.scss"],
+  modules: ["@formkit/nuxt", "@pinia/nuxt", "@nuxtjs/i18n"],
+});
