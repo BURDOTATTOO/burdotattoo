665eeac3ba58f5adad8d3d2779745260f873fa9d
Alejandro Cano <alejandroyunes@outlook.com>
2024-08-10 21:07:51 -0500

    added hover in deskto menu

--- components/menus/dropdown/desktop/index.vue
+++ components/menus/dropdown/desktop/index.vue
@@ -12,31 +12,29 @@ import WebDesignSvg from "~/components/icons/menu/WebDesignSvg.vue"
 import SuggestionsSvg from "~/components/icons/menu/SuggestionsSvg.vue"
 import AnnouncementsSvg from '~/components/icons/menu/AnnouncementsSvg.vue'
 
-const showServices = ref<boolean | null>(null)
-const showSupport = ref<boolean | null>(null)
+const services = ref<boolean | null>(null)
+const support = ref<boolean | null>(null)
 const logged = ref(false)
 const loadingProfile = ref(false)
 
-const toggleDropdown = () => {
-  showServices.value = !showServices.value
-  showSupport.value = false
+const showDropdown = () => {
+  services.value = true
+  support.value = false
 }
 
-const toggleSupport = () => {
-  showSupport.value = !showSupport.value
-  showServices.value = false
+const hideDropdown = () => {
+  services.value = false
+  support.value = false
 }
 
-const handleLogin = () => {
-  console.log('Login')
-  showServices.value = false
+const showSupport = () => {
+  support.value = true
+  services.value = false
+}
 
-  loadingProfile.value = true
-  setTimeout(() => {
-    loadingProfile.value = false
-    logged.value = !logged.value
-  }, 2000)
-
+const hideSupport = () => {
+  support.value = false
+  services.value = false
 }
 
 </script>
@@ -46,32 +44,32 @@ const handleLogin = () => {
   <nav class="menu desktop-only">
     <ul class="items">
 
-      <li class="item" @click="toggleDropdown">
+      <li class="item" @mouseenter="showDropdown">
         <h3>Servicios</h3>
-        <ArrowDownSvg :class="['icon arrow-icon', showServices ? 'rotate-opposite' : 'rotate']" />
+        <ArrowDownSvg :class="['icon arrow-icon', services ? 'rotate-opposite' : 'rotate']" />
 
-        <ul class="sub-items" v-if="showServices"> 
-          <NuxtLink to="/diseno-web" class="sub-item">
+        <ul class="sub-items" v-if="services" @mouseleave="hideDropdown"> 
+          <NuxtLink to="/diseno-web" class="sub-item" @click="hideDropdown">
             <WebDesignSvg class="icon" />
             <p>Diseño web</p>
           </NuxtLink>
 
-          <NuxtLink to="#" class="sub-item">
+          <NuxtLink to="#" class="sub-item" @click="hideDropdown">
             <GraphicDesignSvg class="icon" />
             <p>Diseño de gráfico</p>
           </NuxtLink>
 
-          <NuxtLink to="#" class="sub-item">
+          <NuxtLink to="#" class="sub-item" @click="hideDropdown">
             <ServerSvg class="icon" />
             <p>Alojamiento web</p>
           </NuxtLink>
 
-          <NuxtLink to="/marketing-online" class="sub-item">
+          <NuxtLink to="/marketing-online" class="sub-item" @click="hideDropdown">
             <SeoSvg class="icon" />
             <p>Marketing de motores de búsqueda</p>
           </NuxtLink>
 
-          <NuxtLink to="/marketing-en-redes-sociales" class="sub-item">
+          <NuxtLink to="/marketing-en-redes-sociales" class="sub-item" @click="hideDropdown">
             <MarketingSvg class="icon comment-icon" />
             <p>Marketing de medios sociales</p>
           </NuxtLink>
@@ -79,22 +77,22 @@ const handleLogin = () => {
 
       </li>
 
-      <li class="item" @click="toggleSupport">
+      <li class="item" @mouseenter="showSupport">
         <h3>Soporte</h3>
-        <ArrowDownSvg :class="['icon arrow-icon', showSupport ? 'rotate-opposite' : 'rotate']" />
+        <ArrowDownSvg :class="['icon arrow-icon', support ? 'rotate-opposite' : 'rotate']" />
 
-        <ul class="sub-items" v-if="showSupport">
-          <NuxtLink to="#" class="sub-item">
+        <ul class="sub-items" v-if="support" @mouseleave="hideDropdown">
+          <NuxtLink to="#" class="sub-item" @click="hideDropdown">
             <SupportSvg class="icon" />
             <p>Contacta con soporte</p>
           </NuxtLink>
 
-          <NuxtLink to="#" class="sub-item">
+          <NuxtLink to="#" class="sub-item" @click="hideDropdown">
             <AnnouncementsSvg class="icon" />
             <p>Anuncios</p>
           </NuxtLink>
 
-          <NuxtLink to="#" class="sub-item">
+          <NuxtLink to="#" class="sub-item" @click="hideDropdown">
             <SuggestionsSvg class="icon" />
             <p>Sugerencias</p>
           </NuxtLink>
@@ -107,6 +105,6 @@ const handleLogin = () => {
   </nav>
 
 
-  <div @click="toggleDropdown" :class="{ 'menu-dropdown-bg': showServices }"></div>
-  <div @click="toggleSupport" :class="{ 'menu-dropdown-bg': showSupport }"></div>
+  <div @click="hideDropdown" :class="{ 'menu-dropdown-bg': services || support }"></div>
+  <div @click="showSupport" :class="{ 'menu-dropdown-bg': support }"></div>
 </template>
