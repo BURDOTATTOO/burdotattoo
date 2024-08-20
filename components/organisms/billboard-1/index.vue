ba5922e4a91e5329ee89ec00d8118f156282a6e1
Alejandro Cano <alejandroyunes@outlook.com>
2024-08-19 23:19:30 -0500

    fix billboard-1

--- components/organisms/billboard-1/index.vue
+++ components/organisms/billboard-1/index.vue
@@ -4,7 +4,7 @@ import './billboard-1.scss'
 </script>
 
 <template>
-  <article class="billboard-1">
+  <section class="billboard-1">
 
     <NuxtPicture
       class="desktop-only"
@@ -13,29 +13,28 @@ import './billboard-1.scss'
       format="webp"
       alt="Imagen de diseño web portfolio" />
 
-    <section class="details-wrapper">
+    <div class="details-wrapper">
       <div class="details">
-        <h1>
+        <h1 class="title">
           Expertos en Diseño Web - Servicio Profesional
         </h1>
-        <h2>
+        <h2 class="subtitle">
           soluciones web personalizadas.
         </h2>
-        <p>Somos una empresa líder en diseño web y desarrollo digital en Colombia, ofreciendo soluciones profesionales
+        <p class="description">
+          Somos una empresa líder en diseño web y desarrollo digital en Colombia, ofreciendo soluciones profesionales
           para pequeñas empresas, corporaciones y necesidades individuales. Creamos sitios web destacados que impulsan
           el crecimiento orgánico y fortalecen su presencia digital.
         </p>
-        <p>
+        <p class="description">
           Nos especializamos en diseño web responsivo, desarrollo de aplicaciones web, SEO y marketing digital integral.
           Nuestro objetivo es atraer más atención a su sitio y establecer una posición dominante en su industria.
         </p>
-
-        <p>
+        <p class="description">
           Los beneficios de nuestro trabajo incluyen aumento del tráfico orgánico, mejora de la experiencia del usuario,
           incremento en las conversiones y fortalecimiento de la imagen de marca.
         </p>
-
       </div>
-    </section>
-  </article>
+    </div>
+  </section>
 </template>
