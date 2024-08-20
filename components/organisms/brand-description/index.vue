f9abd6c9c043e032c28029a1581d9c461d054ddb
Alejandro Cano <alejandroyunes@outlook.com>
2024-08-19 23:11:58 -0500

    fix brand

--- components/organisms/brand-description/index.vue
+++ components/organisms/brand-description/index.vue
@@ -7,15 +7,15 @@ import './brand-description.scss'
   <section class="brand-description">
     <div class="items">
       
-      <div class="item">
+      <div>
         <p class="title">Fundada en 2016</p>
 
-        <h2>Agencia Colombiana de marketing que ofrece soluciones efectivas y personalizadas
+        <h2 class="description">Agencia Colombiana de marketing que ofrece soluciones efectivas y personalizadas
           a empresas de <span>cualquier tamaño.</span></h2>
 
       </div>
 
-      <p class="item">Nuestros servicios de agencia de marketing incluyen diseño de sitios web, marketing en redes
+      <p>Nuestros servicios de agencia de marketing incluyen diseño de sitios web, marketing en redes
         sociales,
         aplicaciones de inteligencia artificial, alojamiento de sitios web, registro de dominios, marketing en motores
         de búsqueda, diseño gráfico, artículos promocionales e impresión. Nos dedicamos a brindar soluciones de
@@ -23,7 +23,7 @@ import './brand-description.scss'
         clientes.
       </p>
 
-      <p class="item">
+      <p>
         Como equipo, nos esforzamos por alcanzar la excelencia con el más alto nivel de integridad y satisfacción del
         cliente. Explore nuestra lista de servicios que ofrecemos y comuníquese con nuestro equipo hoy mismo para
         cualquier consulta que pueda tener.
